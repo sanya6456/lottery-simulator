a2b121a518fb6b80da96c01fd8db99b41d1d04f6
sanya6456 <sanya.benkei@gmail.com>
2026-03-28 23:58:36 +0100

    feat: add Header component with gradient styling

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import PlusIcon from "../../icons/PlusIcon";
+import Heading from "../heading/Heading";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header className="header-gradient h-15 w-full flex items-center px-5 mb-6 lg:mb-12">
+      <div className="flex items-center">
+        <PlusIcon className="mr-5" aria-hidden="true" />
+        <Heading className="text-white">Lottery simulator</Heading>
+      </div>
+    </header>
+  );
+}
